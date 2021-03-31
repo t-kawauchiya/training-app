899adafd6097b53f84da3e1b5742b01defa378b3
sendaitanitakeshi <t.kawauchiya@gmail.com>
2021-03-31 14:52:18 +0900

    ログインページのデザインを更新

--- lib/signIn/sign_in_page.dart
+++ lib/signIn/sign_in_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:provider/provider.dart';
 import 'package:training_app/presentation/signUp/sign_up_page.dart';
 import 'package:training_app/signIn/sign_in_model.dart';
@@ -12,55 +13,100 @@ class SignInPage extends StatelessWidget {
     return ChangeNotifierProvider<SignInModel>(
       create: (_) => SignInModel(),
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('ログイン'),
-        ),
+        appBar: AppBar(),
         body: Consumer<SignInModel>(
           builder: (context, model, child) {
             return Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'example@example.com',
+                  Container(
+                    width: 300,
+                    height: 200,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: AssetImage('images/machoman.jpeg'))),
+                  ),
+                  Text(
+                    'Let`s training',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 45,
+                      color: Colors.amber,
                     ),
-                    obscureText: false,
-                    controller: mailController,
-                    onChanged: (text) {
-                      model.mail = text;
-                    },
                   ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'パスワード',
+                  Center(
+                    child: Container(
+                      width: 300,
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: 'example@example.com',
+                            ),
+                            obscureText: false,
+                            controller: mailController,
+                            onChanged: (text) {
+                              model.mail = text;
+                            },
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: 'パスワード',
+                            ),
+                            obscureText: true,
+                            controller: passwordController,
+                            onChanged: (text) {
+                              model.password = text;
+                            },
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            child: SignInButtonBuilder(
+                              text: 'Sign in with Email',
+                              textColor: Colors.black87,
+                              icon: Icons.email,
+                              backgroundColor: Colors.amber,
+                              onPressed: () async {
+                                model.signInWithEmail(context);
+                              },
+                            ),
+                          ),
+                          SizedBox(
+                            width: 300,
+                            child: SignInButton(
+                              Buttons.Google,
+                              onPressed: () async {
+                                model.signInWithGoogle();
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                TextButton(
+                                  child: Text('create account'),
+                                  style: TextButton.styleFrom(
+                                    primary: Colors.amber,
+                                  ),
+                                  onPressed: () async {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => SignUpPage()),
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    obscureText: true,
-                    controller: passwordController,
-                    onChanged: (text) {
-                      model.password = text;
-                    },
-                  ),
-                  ElevatedButton(
-                    child: Text('ログインする'),
-                    onPressed: () async {
-                      model.signInWithEmail(context);
-                    },
-                  ),
-                  ElevatedButton(
-                    child: Text('新規登録'),
-                    onPressed: () async {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SignUpPage()),
-                      );
-                    },
-                  ),
-                  ElevatedButton(
-                    child: Text('sign in with Google account'),
-                    onPressed: () async {
-                      model.signInWithGoogle();
-                    },
                   ),
                 ],
               ),
