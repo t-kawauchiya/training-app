89d35f5597365ecc9e0a6377e559f466070cc8e7
sendaitanitakeshi <t.kawauchiya@gmail.com>
2021-04-20 17:30:26 +0900

    modified User profile

--- lib/domain/user.dart
+++ lib/domain/user.dart
@@ -2,18 +2,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class User {
   final String title;
-  final Timestamp date;
+  final Timestamp createdAt;
   final DocumentReference? reference;
 
   User.fromMap(Map<String, dynamic> map, {this.reference})
       : assert(map['title'] != null),
         assert(map['date'] != null),
         title = map['title'],
-        date = map['date'];
+        createdAt = map['createdAt'];
 
   User.fromSnapshot(DocumentSnapshot snapshot)
       : this.fromMap(snapshot.data()!, reference: snapshot.reference);
 
   @override
-  String toString() => "Record<$title:$date>";
+  String toString() => "Record<$title:$createdAt>";
 }
